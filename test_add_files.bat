4851870555033528b7f8d318f0bc8eddc4fee59e
gnubrew <b.greifen@gmail.com>
2025-02-08 18:23:10 +0100

    o) Adjusted number of text files created from 10 to 100 in `test_add_files.bat`
    The script now generates 100 files instead of 10 to possibly meet updated requirements or test larger workloads. This ensures consistency with any scaling tests or new functionality demands.

--- test_add_files.bat
+++ test_add_files.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo Creating 10 text files in .\pipeline\10_example_step_one\
-for /l %%i in (1,1,10) do (
+echo Creating 100 text files in .\pipeline\10_example_step_one\
+for /l %%i in (1,1,100) do (
     echo This is file %%i > ".\pipeline\10_example_step_one\text%%i.txt"
     echo Created text%%i.txt
     timeout /t 1 /nobreak >nul
