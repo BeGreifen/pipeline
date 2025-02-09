65164e2487c47b9b7db50f638da43613960312fe
gnubrew <b.greifen@gmail.com>
2025-02-09 08:49:49 +0100

    +) Added detailed docstrings for `log_exceptions_with_args` and `main` functions o) Improved log messages in the `main` function for better clarity o) cosmetics: Fixed minor formatting and PEP compliance issues
    These changes improve code readability and maintainability by enhancing inline documentation, better organizing log messages, and aligning with PEP standards for clarity. This enhances debugging efficiency and code structure.

--- processes/pipeline_step_10_example_step_one.py
+++ processes/pipeline_step_10_example_step_one.py
@@ -2,10 +2,9 @@ import logging
 import functools
 import utils.file_ops as file_ops
 from pathlib import Path
-import setup.logging_setup as logging_setup # Function to initialise logger
+import setup.logging_setup as logging_setup  # Function to initialise logger
 from setup import config_setup  # Interfaces with config.ini functionalities
-import random
-import time
+
 from utils.cache_utils import cache_function
 from processes import process_step_mockup
 
@@ -13,21 +12,41 @@ from processes import process_step_mockup
 config = config_setup.get_prod_config()
 script_name: str = Path(__file__).stem
 
-
 # Build the absolute path for the log file
 logs_dir: Path = logging_setup.configure_logs_directory()
 logfile_path: Path = Path(logs_dir, f"{script_name}.log")
 
-
 # Get the logger instance
 logger = logging_setup.get_logger(
-            logger_name=script_name,
-            logfile_name=logfile_path,
-            console_level=logging.INFO,
-            file_level=logging.DEBUG
-        )
+    logger_name=script_name,
+    logfile_name=logfile_path,
+    console_level=logging.INFO,
+    file_level=logging.DEBUG
+)
+
 
 def log_exceptions_with_args(func):
+    """
+    Provides a decorator to log exceptions raised by the wrapped functions,
+    including their arguments and traceback information. Ensures errors are
+    documented for debugging while re-raising the exceptions for further handling.
+
+    Parameters
+    ----------
+    func : callable
+        The function to be wrapped by the decorator.
+
+    Returns
+    -------
+    callable
+        The wrapped function that includes logging functionality for exceptions.
+
+    Raises
+    ------
+    Exception
+        Re-raises any exception encountered while executing the wrapped function.
+    """
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
@@ -42,24 +61,35 @@ def log_exceptions_with_args(func):
                 exc_info=True  # This logs the traceback as well.
             )
             raise  # Re-raise the exception after logging.
+
     return wrapper
 
 
 # Placeholder Python script
 @cache_function(maxsize=256)
 @log_exceptions_with_args
-def main(file_path: str):
-    print(f"processing file {file_path}")
-    print(f"the logs are stored: {logfile_path}")
-    logger.debug(f"logger started")
+def main(file_path: str) -> bool:
+    """
+    Process a given file and stores logs about the operation.
 
+    The function performs operations such as resolving the file path, logging the
+    process, calling another function to process the file, and finally moving the
+    processed file to a specific directory for further stages.
+
+    Arguments:
+        file_path (str): The path of the file to be processed.
+
+    Returns:
+        bool: Returns True when the process is completed successfully.
+
+    Raises:
+        Any exceptions raised during the file processing or file moving operations
+        are logged by the @log_exceptions_with_args decorator.
+    """
     file_path = Path(file_path).resolve()
-    logger.debug(f"file path: {file_path}")
-
     processed_dir = file_path.parent.parent / "processed/"
-    logger.debug(f"file path: {file_path}")
-    logger.info(f"process {script_name} for {file_path} started")
 
+    logger.info(f"process {script_name} for {file_path} started, output in {processed_dir}")
 
     # code to process file here:
     # ...
